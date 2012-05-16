29a8895257321d34d28a174eb42eaaac0491ebd7
John Bond <john.r.bond@gmail.com>
2012-05-16 20:31:18 +0200

    add rex matching

--- TPB/TPB/spiders/tpb_spider.py
+++ TPB/TPB/spiders/tpb_spider.py
@@ -3,14 +3,13 @@ from scrapy.selector import HtmlXPathSelector
 from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from TPB.items import TpbItem
+import re
 
 class TpbSpider(CrawlSpider):
-#class TpbSpider(BaseSpider):
 	name = "tpb"
 	allowed_domains = ["thepiratebay.se"]
 	start_urls = [ "http://thepiratebay.se/browse/300/1/3" ]
 	rules = ( 
-		#Rule (SgmlLinkExtractor(restrict_xpaths=('//table[@id="searchResult"]')), callback='parse_item'),
 		Rule (SgmlLinkExtractor(restrict_xpaths=('//td[@colspan="9"]/a[last()]')), callback='parse_item', follow=True),
 	)
 
@@ -20,12 +19,18 @@ class TpbSpider(CrawlSpider):
 		item = TpbItem()
 		links = hxs.select('//td')
                 items = []
+		pattern = re.compile('(linux|bsd|l?gpl|apache|mit|free|isc)', re.IGNORECASE)
                 for link in links:
                         item = TpbItem()
                         item['name'] = link.select('div/a/text()').extract()
                         if not item['name']:
                                 continue
+			match = pattern.match(item['name'][0])
+			if not match:
+				continue
+			item['match'] = match.group()
+                        if not item['match']:
+                                continue
                         item['magnet'] = link.select('a[@title="Download this torrent using magnet"]/@href').extract()
-                        print item['name'] , item['magnet']
                         items.append(item)
                 return items
