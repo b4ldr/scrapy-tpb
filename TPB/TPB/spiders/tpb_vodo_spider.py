cf1acdfc3ce91c847efb7cea8bdf2fa3536821a5
John Bond <john.r.bond@gmail.com>
2012-05-24 11:19:53 +0200

    Added extra spider to look for vodo hashs on TPB site

--- TPB/TPB/spiders/tpb_vodo_spider.py
+++ TPB/TPB/spiders/tpb_vodo_spider.py
@@ -4,39 +4,38 @@ from scrapy.contrib.spiders import CrawlSpider, Rule
 from scrapy.contrib.linkextractors.sgml import SgmlLinkExtractor
 from TPB.tpb_items import TpbItem
 import re
+import json
 
 class TpbSpider(CrawlSpider):
-	name = "tpb"
+	name = "tpb_vodo"
 	allowed_domains = ["thepiratebay.se"]
-	start_urls = [ "http://thepiratebay.se/browse/201/" ]
+	start_urls = [ "http://thepiratebay.se/search/vodo/0/" ]
 	rules = ( 
-		Rule (SgmlLinkExtractor(restrict_xpaths=('//td[@colspan="9"]/a[last()]')), callback='parse_item', follow=True),
+		#Rule (SgmlLinkExtractor(restrict_xpaths=('//td[@colspan="9"]/a[last()]')), callback='parse_item', follow=True),
+		Rule (SgmlLinkExtractor(restrict_xpaths=('//div[@align="center"]')), callback='parse_item', follow=True),
 	)
 
 	def parse_item(self, response):
+	        vodo_json = json.loads(open("/home/jbond/json").read())
 		hxs = HtmlXPathSelector(response)
 		items = []
 		item = TpbItem()
 		links = hxs.select('//td')
                 items = []
-		#software = re.compile('(linux|bsd|l?gpl|apache|mit|free|isc)', re.IGNORECASE)
-		software = re.compile('(vodo)', re.IGNORECASE)
 		btih = re.compile("urn:btih:([a-f0-9]+)")
                 for link in links:
                         item = TpbItem()
                         item['name'] = link.select('div/a/text()').extract()
                         if not item['name']:
                                 continue
-			match = software.search(item['name'][0])
-			if not match:
-				continue
-			item['match'] = match.group()
-                        if not item['match']:
-                                continue
                         item['magnet'] = link.select('a[@title="Download this torrent using magnet"]/@href').extract()
 			match = btih.search(item['magnet'][0])
-			print item['magnet'][0]
 			if match:
 				item['btih'] = match.group(1)
-                        items.append(item)
+			for bith_vodo in vodo_json['hashlist']:
+				if item['btih'] ==  bith_vodo['bith']:
+                        		items.append(item)
+					print item['magnet'][0]
+					break
+					
                 return items
